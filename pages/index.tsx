27dcc5863129232f7becff42450b980e55137bb3
ua2354223-sketch <ua2354223@gmail.com>
2025-12-24 10:38:06 +0300

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{textAlign: "center", marginTop: 50}}>
+      <h1>🎉 Website Successfully Connected</h1>
+      <p>Your Next.js website is now running on Vercel.</p>
+    </div>
+  );
+}
